60a696b9347b27003a80997a39c50f026cd08ec8
Kamil Kuzara <kamilkuzara@gmail.com>
2023-03-10 22:55:05 +0100

    Update the get_help() function

--- main/src/utils/help.py
+++ main/src/utils/help.py
@@ -1,8 +1,10 @@
+from solver import algorithms
+
 def get_help():
-    help_text = "Usage: python3 main.py  <problem_file>  [algorithm_code]  [solution_file]\n\n"
-    # help_text += "Available algorithms:\n"
-    #
-    # for algorithm_code, algorithm in algorithms.algorithms.items():
-    #     help_text += "\t" + algorithm_code + " - " + algorithm.get("description", "No description provided") + "\n"
+    help_text = "Usage: python3.7 main.py  <problem_file>  [solution_method]  [solution_file]\n\n"
+    help_text += "Available solution methods:\n"
+
+    for algorithm_code, algorithm in algorithms.items():
+        help_text += "\t" + algorithm_code + " - " + algorithm.get("description", "No description provided or out-of-use. See the documentation for details") + "\n"
 
     return help_text
