5a30530ca16670128ae013d462374066386c05db
Kamil Kuzara <kamilkuzara@gmail.com>
2023-03-18 23:18:26 +0100

    Implement the get_max_cost_difference() function

--- main/src/problem/SBRP_Solution.py
+++ main/src/problem/SBRP_Solution.py
@@ -26,4 +26,14 @@ class SBRP_Solution(Solution):
         pass
 
     def get_max_cost_difference(self):
-        pass
+        if len(self._problem.distance_matrix) == 1:
+            return 0
+
+        # flatten the distance matrix into a list of distances
+        distances = [ dist for row in self._problem.distance_matrix for dist in row ]
+
+        # sort the list in ascending order
+        distances = sorted(distances)
+
+        # compute the cost difference by subtracting the two shortest distances from the two longest
+        return distances[len(distances) - 1] + distances[len(distances) - 2] - distances[0] - distances[1]
